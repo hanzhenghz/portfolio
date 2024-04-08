7c4a6f3025b1e18cf4c40b009e39358db90da6ba
hzheng102719 <hzheng102719@gmail.com>
2024-04-08 11:50:37 -0400

    i18next

---
+++ js/i18n-setup.js
@@ -0,0 +1,19 @@
+i18next
+  .use(i18nextXHRBackend)
+  .init({
+    lng: 'zh',
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json'
+    }
+  }, function(err, t) {
+    if (err) {
+      console.log('Error initializing i18next:', err);
+      return;
+    }
+    // update all elements with a `data-i18n` attribute
+    var elems = document.querySelectorAll('[data-i18n]');
+    elems.forEach(function(elem) {
+      var key = elem.getAttribute('data-i18n');
+      elem.textContent = i18next.t(key);
+    });
+  });
