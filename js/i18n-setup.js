c546aa7112a61fb4600239732677d0b794926919
hzheng102719 <hzheng102719@gmail.com>
2024-06-09 21:30:57 +0800

    i18n fin

--- js/i18n-setup.js
+++ js/i18n-setup.js
@@ -46,21 +46,7 @@ loadScript('https://cdnjs.cloudflare.com/ajax/libs/i18next-xhr-backend/3.2.2/i18
       }
 
       document.addEventListener('DOMContentLoaded', (event) => {
-        // Get the language select buttons
-        var selectEnglish = document.getElementById('selectEnglish');
-        var selectChinese = document.getElementById('selectChinese');
-    
-        // Get the previously selected language from localStorage
-        var selectedLanguage = localStorage.getItem('selectedLanguage');
-    
-        // Update the appearance of the buttons based on the selected language
-        if (selectedLanguage === 'en') {
-            selectEnglish.classList.add('selected');
-            selectChinese.classList.remove('selected');
-        } else if (selectedLanguage === 'zh') {
-            selectChinese.classList.add('selected');
-            selectEnglish.classList.remove('selected');
-        }
+
     
         // Add event listeners to the buttons
         if (selectEnglish) {
@@ -91,6 +77,22 @@ loadScript('https://cdnjs.cloudflare.com/ajax/libs/i18next-xhr-backend/3.2.2/i18
                 selectChinese.classList.add('selected');
                 selectEnglish.classList.remove('selected');
             }
+
+                    // Get the language select buttons
+        var selectEnglish = document.getElementById('selectEnglish');
+        var selectChinese = document.getElementById('selectChinese');
+    
+        // Get the previously selected language from localStorage
+        var selectedLanguage = localStorage.getItem('selectedLanguage');
+    
+        // Update the appearance of the buttons based on the selected language
+        if (selectedLanguage === 'en') {
+            selectEnglish.classList.add('selected');
+            selectChinese.classList.remove('selected');
+        } else if (selectedLanguage === 'zh') {
+            selectChinese.classList.add('selected');
+            selectEnglish.classList.remove('selected');
+        }
     
             localStorage.setItem('selectedLanguage', language);
         }
