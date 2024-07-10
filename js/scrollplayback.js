a3cbec0a705d3565e139e6de16348fbbf3802182
hzheng102719 <hzheng102719@gmail.com>
2024-07-10 17:09:21 +0800

    topbtn

--- js/scrollplayback.js
+++ js/scrollplayback.js
@@ -1,6 +1,8 @@
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    document.body.scrollLeft = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollLeft = 0; // For Chrome, Firefox, IE and Opera
 }
 
 function throttle(func, limit) {
--- js/topbtn.js
+++ js/topbtn.js
@@ -1,10 +1,11 @@
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
+  document.body.scrollLeft = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollLeft = 0; // For Chrome, Firefox, IE and Opera
 }
 
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("topbtn").style.display = "block";
