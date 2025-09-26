4945795d66f767129ebda9ee534ba1d44e936d00
hzheng102719 <hzheng102719@gmail.com>
2025-09-26 08:25:53 -0400

    i18n update

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var footerDate = document.querySelector('#footer .footer-content p');
     if (footerDate) {
-        footerDate.textContent = ` - 2024 Han Zheng`; // This sets the text content to the current year and your name.
+        footerDate.textContent = ` - 2025 Han Zheng`; // This sets the text content to the current year and your name.
     }
 });
