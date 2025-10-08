8c7b3b74ed3d0674665014c53006b52f631f5d7a
hzheng102719 <hzheng102719@gmail.com>
2025-10-08 15:09:33 -0400

    image optimize corpse

--- js/auto-srcset.js
+++ js/auto-srcset.js
@@ -6,6 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
         
         // Generate srcset for your 3 fixed sizes
         img.srcset = `${basePath}/1280.jpg 1280w, ${basePath}/1920.jpg 1920w, ${basePath}/3840.jpg 3840w`;
-        img.sizes = "(max-width: 1280px) 100vw, (max-width: 1920px) 100vw, 100vw";
+        img.sizes = img.dataset.sizes || "(max-width: 1280px) 100vw, (max-width: 1920px) 100vw, 100vw";
     });
 });
